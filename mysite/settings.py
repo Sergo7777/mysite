939dca4b59728d6dde17bffff46552c09698238e
Sergo7777 <serega_denisenko@mail.ru>
2017-07-07 22:44:49 +0300

    t

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,7 +149,8 @@ WEBPACK_LOADER = {
 }
 
 import dj_database_url
-DATABASES = { 'default': dj_database_url.config() }
+DATABASES['default'] = dj_database_url.config()
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
