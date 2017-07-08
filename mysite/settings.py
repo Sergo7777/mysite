50c20d5d739673917dc36f072b4277cd70bc1b5c
Sergo7777 <serega_denisenko@mail.ru>
2017-07-08 11:01:49 +0300

    t

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,12 +148,12 @@ WEBPACK_LOADER = {
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
     }
 }
+
 import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
-import dj_database_url
-import os
-if os.getcwd() == "/app":
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = { 'default': dj_database_url.config() }
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
