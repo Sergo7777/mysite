fc339fe874261bb905b0c153568fde05aae866b6
Sergo7777 <serega_denisenko@mail.ru>
2017-07-08 08:33:23 +0300

    t

--- mysite/settings.py
+++ mysite/settings.py
@@ -150,9 +150,13 @@ WEBPACK_LOADER = {
     }
 }
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
 
 try:
     from .local_settings import *
