d998e85acff0217adf193e75e536f976b7e83c83
Sergo7777 <serega_denisenko@mail.ru>
2017-07-10 16:50:56 +0300

    t

--- mysite/settings.py
+++ mysite/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
         'NAME': 'bar',
         'USER': 'admin',
         'PASSWORD': 'admin111',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': '',
+        'PORT': '',
     }
 }
 TIME_ZONE = None
